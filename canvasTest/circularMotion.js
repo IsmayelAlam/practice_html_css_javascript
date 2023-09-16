afe536d2283bca6641f6e19effe0a282176a743f
Ismayel Alam <ismayelalam0@gmail.com>
2023-09-16 22:58:56 +0600

    finish circular motion funtion

--- canvasTest/circularMotion.js
+++ canvasTest/circularMotion.js
@@ -6,7 +6,7 @@ window.addEventListener("mousemove", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
 });
-window.addEventListener("mouseout", (e) => {
+window.addEventListener("mouseout", () => {
   mouse.x = undefined;
   mouse.y = undefined;
 });
@@ -17,33 +17,64 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
 class Particles {
-  constructor(x, y, dx, dy, radius) {
+  constructor(x, y, v, radius, radians) {
     this.x = x;
     this.y = y;
-    this.dx = dx;
-    this.dy = dy;
+    this.v = v;
     this.radius = radius;
-    this.mass = 1;
+    this.radians = radians;
+    this.lastPos = { x, y };
+    this.distance = Math.random() * 150 + 75;
     this.color = colors[Math.floor(Math.random() * colors.length)];
   }
 
-  draw() {
+  draw(lastPtn) {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.moveTo(lastPtn.x, lastPtn.y);
+    ctx.lineTo(this.x, this.y);
     ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.radius;
     ctx.stroke();
   }
 
   update() {
-    this.draw();
+    const lastPtn = { x: this.x, y: this.y };
+    this.radians += this.v;
+
+    this.lastPos.x += (mouse.x - this.lastPos.x) * 0.05 || 0;
+    this.lastPos.y += (mouse.y - this.lastPos.y) * 0.05 || 0;
+
+    console.log(this.lastPos);
+
+    this.x = Math.cos(this.radians) * this.distance + this.lastPos.x;
+    this.y = Math.sin(this.radians) * this.distance + this.lastPos.y;
+
+    this.draw(lastPtn);
   }
 }
 
-function animateBalls() {
-  requestAnimationFrame(animateBalls);
-  ctx.clearRect(0, 0, innerWidth, innerHeight);
+function init() {
+  const radius = Math.random() * 5 + 2;
+  let x = window.innerWidth / 2;
+  let y = window.innerHeight / 2;
+  let v = Math.random() * 0.05 + 0.01;
+  let radians = Math.random() * Math.PI * 2;
 
-  particlesArr.forEach((particle) => particle.update());
+  return new Particles(x, y, v, radius, radians);
 }
 
-animateBalls();
+const particleArr = [];
+
+for (let i = 0; i < 100; i++) {
+  particleArr.push(init());
+}
+
+function animateCircle() {
+  requestAnimationFrame(animateCircle);
+  ctx.fillStyle = "rgba(0,0,0,0.25";
+  ctx.fillRect(0, 0, innerWidth, innerHeight);
+
+  particleArr.forEach((dot) => dot.update());
+}
+
+animateCircle();
